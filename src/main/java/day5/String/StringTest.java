11dba671f6c1c0764e8fd09607394e4d149e9b8c
ddshpp <dshp753@gmail.com>
2024-05-07 14:59:19 +0900

    create: String 패키지 생성 (내장메소드 실습용)

---
+++ src/main/java/day5/String/StringTest.java
@@ -0,0 +1,29 @@
+package day5.String;
+
+public class StringTest {
+
+    public static void main(String[] args) {
+        createString();
+        stringEqualsTest1();
+        stringLength();
+    }
+
+    private static void stringLength() {
+        String string1 = "아이고아이고~~~";
+        System.out.println(string1.length());
+    }
+
+    private static void stringEqualsTest1() {
+        String string1 = "테스트";
+        String string2 = "테스트";
+        System.out.println(string1.equals(string2));
+        System.out.println(string1==string2);
+    }
+
+    private static void createString() {
+        String string1 = "스트링 1";
+        String string2 = new String("스트링 2");
+        char[] chars = {'스', '트', '링', ' ', '3'};
+        System.out.println(chars);
+    }
+}
