42940746b81aae755d0e53b770e52993d8f350a6
ddshpp <dshp753@gmail.com>
2024-05-07 16:39:37 +0900

    refactor: uppercase utils 메소드 호출 수정

---
+++ src/main/java/day5/String/StringBufferTest.java
@@ -0,0 +1,32 @@
+package day5.String;
+
+public class StringBufferTest {
+    public static void main(String[] args) {
+        stringBufferTest01();
+        stringBufferTest02();
+    }
+
+    private static void stringBufferTest01() {
+        StringBuffer sb1 = new StringBuffer();
+        StringBuffer sb2 = new StringBuffer(100);
+
+        System.out.println(sb1.capacity());
+        System.out.println(sb2.capacity());
+        StringBuffer sb3 = sb2.append("999");
+        System.out.println(sb3);
+        //알고리즘에서 많이 사용했던 기억이 있음
+    }
+
+    private static void stringBufferTest02() {
+        StringBuffer sb1 = new StringBuffer("01");
+        StringBuffer sb2 = sb1.append(23);
+        sb1.append('4').append(56);
+
+        StringBuffer sb3 = sb1.append(78);
+        sb3.append(9.0);
+
+        System.out.println("sb1" + sb1);
+        System.out.println("sb2" + sb2);
+        System.out.println("sb3" + sb3);
+    }
+}
--- src/main/java/day5/uppercase/Application.java
+++ src/main/java/day5/uppercase/Application.java
@@ -1,11 +1,8 @@
 package day5.uppercase;
 
-import static day5.uppercase.Utils.transformation1;
-
 import java.util.Scanner;
 
 public class Application {
-
     static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -15,6 +12,6 @@ public class Application {
     private static void transformationTest() {
         System.out.println("영어 이름 아무거나 입력해주세요");
         String input = SCANNER.nextLine();
-        System.out.println(transformation1(input));
+        System.out.println(Utils.transformation1(input));
     }
 }
