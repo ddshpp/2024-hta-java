dd11622858647b2f5c21a296214b1b05e0eff51f
ddshpp <dshp753@gmail.com>
2024-05-07 17:23:23 +0900

    test: WrapperTest

--- src/main/java/day5/WrapperTest.java
+++ src/main/java/day5/WrapperTest.java
@@ -3,10 +3,18 @@ package day5;
 public class WrapperTest {
     public static void main(String[] args) {
 
-        int maxValue = Integer.MAX_VALUE;
-        int minValue = Integer.MIN_VALUE;
+        Integer maxValue = Integer.MAX_VALUE;
+        Integer minValue = Integer.MIN_VALUE;
+        Integer int1 = Integer.parseInt("1");
+        Integer int2 = Integer.valueOf("2");
 
         System.out.println(maxValue);
         System.out.println(minValue);
+        System.out.println(int1);
+        System.out.println(int2);
+
+        int a = 10;
+        String b = String.valueOf(a);
+        System.out.println(b);
     }
 }
