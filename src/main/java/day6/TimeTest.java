9015d9e5e28670d9d52f2b487aded67f467d2037
ddshpp <dshp753@gmail.com>
2024-05-08 11:34:31 +0900

    test: TimeTest

---
+++ src/main/java/day6/TimeTest.java
@@ -0,0 +1,20 @@
+package day6;
+
+import java.time.LocalDate;
+
+public class TimeTest {
+    public static void main(String[] args) {
+        System.out.println("하이하이");
+
+        //timestamp - 밀리세컨드
+        System.out.println(System.nanoTime());
+        System.out.println(System.currentTimeMillis());
+
+        LocalDate localDate = LocalDate.now();
+        System.out.println("localDate : " + localDate);
+
+        long nanoTime1 = System.nanoTime();
+        long nanoTime2 = System.nanoTime();
+        System.out.println(nanoTime2-nanoTime1);
+    }
+}
