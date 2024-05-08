7190733f9157f7cea1659c71978fc6961f63e23c
ddshpp <dshp753@gmail.com>
2024-05-08 10:48:13 +0900

    feat: 주민번호 입력형식 검사기능

--- src/main/java/day6/residentRegistrationNumber/Application.java
+++ src/main/java/day6/residentRegistrationNumber/Application.java
@@ -2,6 +2,6 @@ package day6.residentRegistrationNumber;
 
 public class Application {
     public static void main(String[] args) {
-
+        Validate.start();
     }
 }
---
+++ src/main/java/day6/residentRegistrationNumber/Validate.java
@@ -0,0 +1,73 @@
+package day6.residentRegistrationNumber;
+
+import java.util.Scanner;
+
+public class Validate {
+
+    public static void start() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("입력해주세요");
+        String input = scanner.nextLine();
+
+        inputLengthCheck(input);
+    }
+
+    public static void inputLengthCheck(String input) {
+        if (input.length() < 14) {
+            throw new RuntimeException("입력값이 너무 짧습니다.");
+        }
+        containsSymbol(input);
+    }
+
+    public static void containsSymbol(String input) {
+        boolean contains = input.contains("-");
+
+        if (!contains) {
+            throw new RuntimeException("입력값에 '-' 이 없습니다");
+        }
+
+        symbolCount(input);
+    }
+
+    public static void symbolCount(String input) {
+        int count = 0;
+
+        for (char c : input.toCharArray()) {
+            if (c == '-') {
+                count++;
+            }
+        }
+
+        if (count != 1) {
+            throw new RuntimeException("입력값에 '-'의 갯수가 많습니다");
+        }
+
+        numberCheck(input);
+    }
+
+    public static void numberCheck(String input) {
+        String[] splits = input.split("-");
+
+        String birthday = splits[0];
+
+        birthdayCheck(birthday, splits);
+
+        System.out.println("올바른 형식으로 입력하였습니다");
+    }
+
+    private static void birthdayCheck(String birthday, String[] splits) {
+        if (birthday.length() != 6) {
+            throw new RuntimeException("생년월일을 제대로 입력해주세요");
+        }
+
+        int month = Integer.parseInt(splits[0].substring(2, 4));
+        int day = Integer.parseInt(splits[0].substring(4, 6));
+
+        if (month > 12) {
+            throw new RuntimeException("없는 월입니다.");
+        }
+        if (day > 31) {
+            throw new RuntimeException("없는 일입니다.");
+        }
+    }
+}
