36a07931764f65431b630f4f5a3f95802047eb6b
ddshpp <dshp753@gmail.com>
2024-05-09 12:13:36 +0900

    test: 인터페이스 상수 불러보기

--- src/main/java/day7/interfaceTest/Application.java
+++ src/main/java/day7/interfaceTest/Application.java
@@ -2,6 +2,15 @@ package day7.interfaceTest;
 
 public class Application {
     public static void main(String[] args) {
+        createObjectImplementsInterface();
+        interfaceConstantsTest();
+    }
+
+    private static void interfaceConstantsTest() {
+        System.out.println(interfaceTest.name);
+    }
+
+    private static void createObjectImplementsInterface() {
         StrongMan strongMan = new StrongMan();
         strongMan.hello();
     }
--- src/main/java/day7/interfaceTest/interfaceTest.java
+++ src/main/java/day7/interfaceTest/interfaceTest.java
@@ -1,5 +1,7 @@
 package day7.interfaceTest;
 
 public interface interfaceTest {
+    String name = "눼헤헤헤";
+
     void hello();
 }
