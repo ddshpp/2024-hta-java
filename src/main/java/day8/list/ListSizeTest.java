6cb7dd5277bfd19719cb2769a02ab45d3d7393e6
ddshpp <dshp753@gmail.com>
2024-05-10 14:10:20 +0900

    test: list size test(내부에서 크기가 바뀌는걸 확인)

---
+++ src/main/java/day8/list/ListSizeTest.java
@@ -0,0 +1,24 @@
+package day8.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListSizeTest {
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
+    }
+}
