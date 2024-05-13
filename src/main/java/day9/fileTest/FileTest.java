b5cddc29345b36777d794864c2bbc79669a18528
ddshpp <dshp753@gmail.com>
2024-05-13 11:19:49 +0900

    feat: 숨김파일여부확인

--- src/main/java/day9/fileTest/FileTest.java
+++ src/main/java/day9/fileTest/FileTest.java
@@ -5,7 +5,15 @@ import java.io.IOException;
 
 public class FileTest {
     public static void main(String[] args) throws IOException {
-        createFile();
+//        createFile();
+        isHidden();
+    }
+
+    private static void isHidden() {
+        String testFilePath = "/Users/sanghoonpark/Desktop/ddshpp/study-hta/file-test/text.txt";
+        File file = new File(testFilePath);
+        boolean isHidden = file.isHidden();
+        System.out.println(isHidden);
     }
 
     private static void createFile() throws IOException {
