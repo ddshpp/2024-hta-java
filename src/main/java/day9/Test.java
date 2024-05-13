65396d782fa2d33274359bd271cac2e86583c101
ddshpp <dshp753@gmail.com>
2024-05-13 09:53:04 +0900

    test: decimalFormat

--- src/main/java/day9/Test.java
+++ src/main/java/day9/Test.java
@@ -1,7 +1,37 @@
 package day9;
 
+import java.text.DecimalFormat;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("테스트");
+        printfTest();
+        decimalFormatTest();
+    }
+
+    private static void printfTest() {
+        System.out.printf("%s-%s", "printf", "테스트");
+    }
+
+    private static void decimalFormatTest() {
+        double number = 1234567.89;
+
+        String[] pattern = {
+                "0",
+                "#",
+                "0.0",
+                "#.#",
+                "0.000",
+                "#.###",
+                "#,###.###",
+                "#E0",
+                "0E0",
+                "#.#%",
+                "¤ #,###"
+        };
+
+        for (String string : pattern) {
+            DecimalFormat df = new DecimalFormat(string);
+            System.out.printf("%10s: %s\n", string, df.format(number));
+        }
     }
 }
