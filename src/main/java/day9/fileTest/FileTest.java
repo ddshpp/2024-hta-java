7d3d057755dcec716b5df47006e1a0ac411064e1
ddshpp <dshp753@gmail.com>
2024-05-13 11:30:43 +0900

    test: getCanonicalFile

--- src/main/java/day9/fileTest/FileTest.java
+++ src/main/java/day9/fileTest/FileTest.java
@@ -6,7 +6,15 @@ import java.io.IOException;
 public class FileTest {
     public static void main(String[] args) throws IOException {
 //        createFile();
-        isHidden();
+//        isHidden();
+        getCanonicalFile();
+    }
+
+    private static void getCanonicalFile() throws IOException {
+        String testFilePath = "/Users/sanghoonpark/Desktop/ddshpp/study-hta/file-test/text.txt";
+        File file = new File(testFilePath);
+        File canonicalFile = file.getCanonicalFile();
+        System.out.println(canonicalFile);
     }
 
     private static void isHidden() {
