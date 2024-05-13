1e9eb70f4e76f919b2b224426de35542f18f3723
ddshpp <dshp753@gmail.com>
2024-05-13 10:07:54 +0900

    test: decimalFormatTest2

--- src/main/java/day9/Test.java
+++ src/main/java/day9/Test.java
@@ -1,18 +1,32 @@
 package day9;
 
 import java.text.DecimalFormat;
+import java.text.ParseException;
 
 public class Test {
     public static void main(String[] args) {
         printfTest();
-        decimalFormatTest();
+        decimalFormatTest1();
+        decimalFormatTest2();
     }
 
-    private static void printfTest() {
-        System.out.printf("%s-%s", "printf", "테스트");
+    private static void decimalFormatTest2() {
+        DecimalFormat decimalFormat1 = new DecimalFormat("#,###.##");
+        DecimalFormat decimalFormat2 = new DecimalFormat("#.###E0");
+        try {
+            Number number1 = decimalFormat1.parse("1,234,567.89");
+            System.out.print("1,234,567.89" + " -> ");
+
+            double number2 = number1.doubleValue();
+            System.out.print(number2 + " -> ");
+
+            System.out.println(decimalFormat2.format(number1));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private static void decimalFormatTest() {
+    private static void decimalFormatTest1() {
         double number = 1234567.89;
 
         String[] pattern = {
@@ -33,5 +47,9 @@ public class Test {
             DecimalFormat df = new DecimalFormat(string);
             System.out.printf("%10s: %s\n", string, df.format(number));
         }
+    }
+
+    private static void printfTest() {
+        System.out.printf("%s-%s", "printf", "테스트");
     }
 }
