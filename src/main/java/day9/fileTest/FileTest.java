02e50ce2728b8274f48cc93bf72f0e0cc2d97ab3
ddshpp <dshp753@gmail.com>
2024-05-13 11:45:59 +0900

    feat: lastModifiedConversion (file.lastModified()를 보기좋게 가공하기)

--- src/main/java/day9/fileTest/FileTest.java
+++ src/main/java/day9/fileTest/FileTest.java
@@ -2,12 +2,40 @@ package day9.fileTest;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 public class FileTest {
     public static void main(String[] args) throws IOException {
 //        createFile();
 //        isHidden();
-        getCanonicalFile();
+//        getCanonicalFile();
+//        printFileSize();
+
+        lastModifiedConversion();
+    }
+
+    private static void lastModifiedConversion() {
+        File file = new File("/Users/sanghoonpark/Desktop/ddshpp/study-hta/file-test/h1.jpeg");
+        long lastModified = file.lastModified();
+        System.out.println("lastModified :" + lastModified);
+
+        Timestamp timestamp = new Timestamp(lastModified);
+        System.out.println("timestamp : " + timestamp);
+
+        LocalDateTime localDateTime = timestamp.toLocalDateTime();
+        System.out.println("localDateTime : " + localDateTime);
+    }
+
+    private static void printFileSize() {
+        String testFilePath1 = "/Users/sanghoonpark/Desktop/ddshpp/study-hta/file-test/text.txt";
+        String testFilePath2 = "/Users/sanghoonpark/Desktop/ddshpp/study-hta/file-test/h1.jpeg";
+        File file1 = new File(testFilePath1);
+        File file2 = new File(testFilePath2);
+        long fileSize1 = file1.length();
+        long fileSize2 = file2.length();
+        System.out.println(fileSize1);
+        System.out.println(fileSize2);
     }
 
     private static void getCanonicalFile() throws IOException {
