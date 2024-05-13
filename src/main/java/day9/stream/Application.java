6f6d47f76f9f493e58f7563ddf11a44ba81482bf
ddshpp <dshp753@gmail.com>
2024-05-13 14:49:50 +0900

    feat: fileInputStream으로 .txt 파일 읽어보기

--- src/main/java/day9/stream/Application.java
+++ src/main/java/day9/stream/Application.java
@@ -1,7 +1,28 @@
 package day9.stream;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("테스트");
+    public static void main(String[] args) throws IOException {
+        String filePath = "/Users/sanghoonpark/Desktop/ddshpp/study-hta/java1/java-1/src/main/java/day9/stream/test.txt";
+
+        fileInputStreamTest(filePath);
+    }
+
+    private static void fileInputStreamTest(String filePath) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream(filePath);
+            int i = 0;
+            while ((i = fileInputStream.read()) != -1) {
+                System.out.print((char) i);
+            }
+            fileInputStream.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("file not found" + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("io exception" + e.getMessage());
+        }
     }
 }
