44e297a78b1de962c3eda339ee24b1ce33c5223e
ddshpp <dshp753@gmail.com>
2024-05-14 15:27:05 +0900

    test: objectIOStreamTes2

--- src/main/java/day10/dataIOStream/Application.java
+++ src/main/java/day10/dataIOStream/Application.java
@@ -1,5 +1,7 @@
 package day10.dataIOStream;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -7,11 +9,56 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application implements Serializable {
     public static void main(String[] args) {
-        dataOutputStreamTest();
-        objectIOStreamTest();
+//        dataOutputStreamTest();
+//        objectIOStreamTest();
+        objectIOStreamTes2();
+    }
+
+    private static void objectIOStreamTes2() {
+        String path = "./src/main/java/day10/dataIOStream/test3.ser";
+
+        try {
+            try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+                    new BufferedOutputStream(new FileOutputStream(path)))) {
+
+                StrongMan strongMan1 = new StrongMan("테스트111", 16, 1000);
+                StrongMan strongMan2 = new StrongMan("테스트222", 16, 2000);
+
+                List<StrongMan> strongManList = new ArrayList<>();
+                strongManList.add(strongMan1);
+                strongManList.add(strongMan2);
+
+                objectOutputStream.writeObject(strongMan1);
+                objectOutputStream.writeObject(strongMan2);
+                objectOutputStream.writeObject(strongManList);
+            }
+            System.out.println("잘끝남요");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(
+                    new BufferedInputStream(new FileInputStream(path)));
+
+            StrongMan object1 = (StrongMan) objectInputStream.readObject();
+            StrongMan object2 = (StrongMan) objectInputStream.readObject();
+            List<StrongMan> objectList = (List<StrongMan>) objectInputStream.readObject();
+
+            object1.getInfo();
+            object2.getInfo();
+            for (StrongMan strongMan : objectList) {
+                strongMan.getInfo();
+            }
+            objectInputStream.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static void objectIOStreamTest() {
--- src/main/java/day10/dataIOStream/StrongMan.java
+++ src/main/java/day10/dataIOStream/StrongMan.java
@@ -3,9 +3,9 @@ package day10.dataIOStream;
 import java.io.Serializable;
 
 public class StrongMan implements Serializable {
-    private String name;
-    private int age;
-    private int power;
+    private final String name;
+    private final int age;
+    private final int power;
 
     public StrongMan(String name, int age, int power) {
         this.name = name;
