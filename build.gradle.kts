24c78a84660a2202614e973df7df7f6f3a60fa51
ddshpp <dshp753@gmail.com>
2024-05-16 09:38:11 +0900

    library: Apache POI Common » 5.2.3, JXL » 2.6.12 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,12 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // https://mvnrepository.com/artifact/org.apache.poi/poi
+    implementation("org.apache.poi:poi:5.2.3")
+
+    // https://mvnrepository.com/artifact/net.sourceforge.jexcelapi/jxl
+    implementation("net.sourceforge.jexcelapi:jxl:2.6.12")
 }
 
 tasks.test {
