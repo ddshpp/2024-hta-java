f4630dcb58fc6432484f3ffb39a80c8484590783
ddshpp <dshp753@gmail.com>
2024-05-16 10:57:42 +0900

    test: 엑셀 파일 읽어보기(.xls, .xlsx)

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,8 @@ dependencies {
     // https://mvnrepository.com/artifact/org.apache.poi/poi
     implementation("org.apache.poi:poi:5.2.3")
 
-    // https://mvnrepository.com/artifact/net.sourceforge.jexcelapi/jxl
-    implementation("net.sourceforge.jexcelapi:jxl:2.6.12")
+    // https://mvnrepository.com/artifact/org.apache.poi/poi-ooxml
+    implementation("org.apache.poi:poi-ooxml:5.2.3")
 }
 
 tasks.test {
---
+++ src/main/java/day11/excel/Application.java
@@ -0,0 +1,47 @@
+package day11.excel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Application {
+    static String test1Path = "src/main/java/day11/excel/test1.xlsx";
+    static String test2Path = "src/main/java/day11/excel/test2.xls";
+
+    public static void main(String[] args) throws Exception {
+//        getCellXlsFile();
+        printXlsxTest();
+    }
+
+    private static void printXlsxTest() throws IOException {
+        try (FileInputStream fis = new FileInputStream(new File(test1Path))) {
+            XSSFWorkbook workbook = new XSSFWorkbook(fis);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            XSSFCell cell = sheet.getRow(0).getCell(0);
+            System.out.println(cell);
+        }
+    }
+
+    private static void getCellXlsFile() {
+        File file = new File(test2Path);
+
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            HSSFWorkbook workbook = new HSSFWorkbook(fis);
+            HSSFSheet sheet = workbook.getSheetAt(0);
+            HSSFCell cell = sheet.getRow(0).getCell(0);
+
+            System.out.println("workbook.getNumberOfSheets() : " + workbook.getNumberOfSheets());
+            System.out.println("sheet.getSheetName() : " + sheet.getSheetName());
+            System.out.println("cell : " + cell);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
